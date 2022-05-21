cf54f0fa7d5ee9541795f706728626f944112975
Gabriel Calistro <gahcalistro@gmail.com>
2022-05-21 20:18:02 -0300

    scrollReveal + readme + fixing tags in html + active and hover style in navigation

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ document.addEventListener("scroll", onScroll);
 onScroll();
 
 function onScroll() {
-  showButton();
+  showBackToTopButton();
+  activeStyleAtCurrentSection(home);
+  activeStyleAtCurrentSection(about);
+  activeStyleAtCurrentSection(services);
+  activeStyleAtCurrentSection(comments);
+  activeStyleAtCurrentSection(contact);
 }
 
 function openMenu() {
@@ -14,7 +19,7 @@ function closeMenu() {
   document.body.classList.remove("menu-expanded");
 }
 
-function showButton() {
+function showBackToTopButton() {
   if (scrollY > 500) {
     backToTopButton.classList.add("showButton");
   } else {
@@ -22,6 +27,32 @@ function showButton() {
   }
 }
 
+function activeStyleAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2;
+
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+  const sectionBottom = sectionTop + sectionHeight;
+  const sectionId = section.getAttribute("id");
+
+  const passedTop = targetLine >= sectionTop;
+  const passedBottom = targetLine >= sectionBottom;
+
+  const sectionBoundaries = passedTop && !passedBottom;
+
+  const menuElement = document.querySelector(
+    `#navigation .menu a[href*=${sectionId}]`
+  );
+
+  menuElement.classList.remove("active");
+
+  if (sectionBoundaries) {
+    menuElement.classList.add("active");
+    console.log(sectionId);
+  }
+}
+
+// SWIPER
 const swiper = new Swiper(".swiper", {
   // Parameters
   direction: "horizontal",
@@ -41,3 +72,20 @@ const swiper = new Swiper(".swiper", {
     },
   },
 });
+
+// SCROLLREVEAL
+ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+}).reveal(`
+  #home,
+  #about,
+  #services,
+  #services p,
+  #services .card,
+  #services .card .cards,
+  #comments,
+  #comments .posts,
+  #contact
+`);
